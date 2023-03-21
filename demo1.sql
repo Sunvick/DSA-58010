9bac5beeff0f90bb4c5c6f079f1d84fc92d78827
Sunvick <98928025+Sunvick@users.noreply.github.com>
2023-03-21 12:26:53 +0800

    Add files via upload

---
+++ demo1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE orderstable(OrderID INT NOT NULL, OrderNumber INT NOT NULL, PRIMARY KEY (OrderID));
+INSERT INTO orderstable(OrderID, OrderNumber)
+				VALUES('1','77895'), 
+					('2','44678'), 
+					('3','22456'); 
